73d52fe103eef47a265e70d9dffe4360d201236b
Mingkai Dong <mk@dong.mk>
2022-03-19 23:16:31 +0800

    add vue3-mindmap

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <mindmap v-model="data"></mindmap>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import mindmap from 'vue3-mindmap'
+import 'vue3-mindmap/dist/style.css'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    name: 'MindMap',
+    components: {
+        mindmap
+    },
+    setup () => {
+        const data = [{
+            "name":"如何学习D3",
+            "children": [
+                {
+                    "name":"预备知识",
+                    "children": [
+                        { "name":"HTML & CSS" },
+                        { "name":"JavaScript" },
+                    ]
+                },
+                {
+                    "name":"安装",
+                    "collapse": true,
+                    "children": [ { "name": "折叠节点" } ]
+        },
+        { "name":"进阶", "left": true },
+      ]
+    }]
+
+    return { data }
   }
 }
 </script>
